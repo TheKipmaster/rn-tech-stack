6c996db68ebc418e9bce1bdb0ddb3c434d21154f
TheKipmaster <fkipman@hotmail.com>
2018-07-13 14:43:23 -0300

    Corrects bug in 'createStore' function call inside App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// import reducers from './reducers';
+import reducers from './reducers';
 import { Header } from './components/common';
 
 const App = () => (
-  <Provider store={createStore}>
+  <Provider store={createStore(reducers)}>
     <View>
       <Header title="Tech Stack" />
     </View>
